07c9e83feada0c2a9b0bbcf01cc745f35067ce11
the_real_farfetchd <git@marco-rebhan.de>
2018-03-30 20:39:12 +0200

    remove duplicate information (mod version, name, ...)

--- src/main/java/mod/crystals/CrystalsMod.java
+++ src/main/java/mod/crystals/CrystalsMod.java
@@ -8,12 +8,10 @@ import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = CrystalsMod.MODID, name = CrystalsMod.NAME, version = CrystalsMod.VERSION)
+@Mod(modid = CrystalsMod.MODID)
 public class CrystalsMod {
 
     public static final String MODID = "crystals";
-    public static final String NAME = "Crystals";
-    public static final String VERSION = "1.0";
 
     @Instance
     public static CrystalsMod instance;
