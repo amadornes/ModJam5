8c78e78204b9fb0ddf988d57f5f0359dbcab745b
the_real_farfetchd <git@marco-rebhan.de>
2018-03-31 14:12:20 +0200

    client sync & save to disk

--- src/main/java/mod/crystals/tile/TileCrystal.java
+++ src/main/java/mod/crystals/tile/TileCrystal.java
@@ -1,42 +1,88 @@
 package mod.crystals.tile;
 
 import mod.crystals.api.IResonant;
+import mod.crystals.capability.CapabilityResonant;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.nbt.NBTBase;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.NetworkManager;
+import net.minecraft.network.play.server.SPacketUpdateTileEntity;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraftforge.common.capabilities.Capability;
 
+import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
 public class TileCrystal extends TileEntity {
 
-    // TODO: Save to NBT
-    // TODO: Sync
     private IResonant.Default resonant = (IResonant.Default) IResonant.CAPABILITY.getDefaultInstance();
+    private static CapabilityResonant.Storage serializer = new CapabilityResonant.Storage();
 
     public TileCrystal() {
         resonant.addChangeListener(this::onChanged);
     }
 
     private void onChanged() {
-        getWorld().markBlockRangeForRenderUpdate(getPos(), getPos());
+        IBlockState state = getWorld().getBlockState(pos);
+        getWorld().notifyBlockUpdate(pos, state, state, 3);
         markDirty();
     }
 
     @Override
     public void validate() {
         super.validate();
-        //resonant.setResonance(1);
-        //resonant.setNatureAmounts(EnvironmentHandler.INSTANCE.getNature(getWorld(), getPos()));
+        // resonant.setResonance(1);
+        // resonant.setNatureAmounts(EnvironmentHandler.INSTANCE.getNature(getWorld(), getPos()));
     }
 
     @Override
-    public boolean hasCapability(Capability<?> capability, @Nullable EnumFacing facing) {
-        return capability == IResonant.CAPABILITY || super.hasCapability(capability, facing);
+    public NBTTagCompound writeToNBT(NBTTagCompound compound) {
+        super.writeToNBT(compound);
+        NBTBase tag = serializer.writeNBT(IResonant.CAPABILITY, resonant, null);
+        compound.setTag("rd", tag);
+        return compound;
+    }
+
+    @Override
+    public void readFromNBT(NBTTagCompound compound) {
+        super.readFromNBT(compound);
+        NBTBase tag = compound.getTag("rd");
+        if (tag != null) serializer.readNBT(IResonant.CAPABILITY, resonant, null, tag);
     }
 
     @Nullable
     @Override
-    public <T> T getCapability(Capability<T> capability, @Nullable EnumFacing facing) {
+    public SPacketUpdateTileEntity getUpdatePacket() {
+        return new SPacketUpdateTileEntity(getPos(), 0, getUpdateTag());
+    }
+
+    @Override
+    @Nonnull
+    public NBTTagCompound getUpdateTag() {
+        return writeToNBT(new NBTTagCompound());
+    }
+
+    @Override
+    public void handleUpdateTag(@Nonnull NBTTagCompound tag) {
+        readFromNBT(tag);
+        getWorld().markBlockRangeForRenderUpdate(getPos(), getPos());
+    }
+
+    @Override
+    public void onDataPacket(NetworkManager net, SPacketUpdateTileEntity pkt) {
+        handleUpdateTag(pkt.getNbtCompound());
+    }
+
+    @SuppressWarnings("ConstantConditions")
+    @Override
+    public boolean hasCapability(@Nonnull Capability<?> capability, @Nullable EnumFacing facing) {
+        return capability == IResonant.CAPABILITY || super.hasCapability(capability, facing);
+    }
+
+    @SuppressWarnings({"ConstantConditions", "unchecked"})
+    @Override
+    public <T> T getCapability(@Nonnull Capability<T> capability, @Nullable EnumFacing facing) {
         if (capability == IResonant.CAPABILITY) return (T) resonant;
         return super.getCapability(capability, facing);
     }
